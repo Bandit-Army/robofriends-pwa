31475844c29e400f87a3426e6e09784b5c64fee1
Joe Lee <joe.p.lee@outlook.com>
2024-12-20 10:55:22 -0600

    updated serviceworker

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import 'tachyons';
 
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -36,5 +36,5 @@ root.render(
 //   </Provider>,
 //   document.getElementById('root')
 // );
-registerServiceWorker();
+serviceWorker.register();
 
